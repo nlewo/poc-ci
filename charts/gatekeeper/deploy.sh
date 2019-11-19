4b62cd2754a46088aa5579fd57d5c80c004de01b
Antoine Eiche <lewo@abesis.fr>
2019-11-19 12:10:47 +0100

    gatekeeper: init

---
+++ charts/gatekeeper/deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+helm template --values=values.yaml  .  --set withGatekeeperConstraintsCRDs=false | kapp deploy -a gatekeeper -y -f -
+
+echo "Waiting for cirules.constraints.gatekeeper.sh crd creation"
+while ! kubectl get crd cirules.constraints.gatekeeper.sh;
+do
+    sleep 1
+done
+
+# helm template --values=values.yaml  . | kapp deploy -a gatekeeper -y -f -
